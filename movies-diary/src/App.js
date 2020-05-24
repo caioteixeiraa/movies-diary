961d2c83dfc0f3c2faceca15a665e3a590014602
caioteixeiraa <tex.caio@gmail.com>
2020-05-23 23:02:54 -0300

    add header font

--- movies-diary/src/App.js
+++ movies-diary/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="header">Calendário de filmes</h1>
+      <h1 className="header">Calendario de filmes</h1>
     </div>
   );
 }
